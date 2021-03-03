3c245a286444dcddfc8ad44b07b368ab71465df7
Abiel Figueroa <Abe@zipcodes-MacBook-Pro-2.local>
2021-03-02 21:47:28 -0500

    Board Class Complete

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,22 +4,72 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] gameBoard;
     public Board(Character[][] matrix) {
+
+        this.gameBoard=matrix;
     }
 
-    public Boolean isInFavorOfX() {
-        return null;
+    public Boolean isInFavorOfX() {{
+        if(gameBoard[0][0]=='X') {
+            if(gameBoard[0][1]=='X'&&gameBoard[0][2]=='X'){return true;}
+            if(gameBoard[1][1]=='X'&&gameBoard[2][2]=='X'){return true;}
+            if(gameBoard[1][0]=='X'&&gameBoard[2][0]=='X'){return true;}
+        }
+        if(gameBoard[2][0]=='X') {
+            if(gameBoard[2][1]=='X'&&gameBoard[2][2]=='X'){return true;}
+            if(gameBoard[1][1]=='X'&&gameBoard[0][2]=='X'){return true;}
+        }
+        if(gameBoard[1][1]=='X') {
+            if(gameBoard[0][1]=='X'&&gameBoard[2][1]=='X'){return true;}
+            if(gameBoard[1][0]=='X'&&gameBoard[1][2]=='X'){return true;}
+        }
+        if(gameBoard[0][2]=='X') {
+            if(gameBoard[1][2]=='X'&&gameBoard[2][2]=='X'){return true;}
+        }
+        return false;
+    }
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if(gameBoard[0][0]=='O') {
+            if(gameBoard[0][1]=='O'&&gameBoard[0][2]=='O'){return true;}
+            if(gameBoard[1][1]=='O'&&gameBoard[2][2]=='O'){return true;}
+            if(gameBoard[1][0]=='O'&&gameBoard[2][0]=='O'){return true;}
+        }
+        if(gameBoard[2][0]=='O') {
+            if(gameBoard[2][1]=='O'&&gameBoard[2][2]=='O'){return true;}
+            if(gameBoard[1][1]=='O'&&gameBoard[0][2]=='O'){return true;}
+        }
+        if(gameBoard[1][1]=='O') {
+            if(gameBoard[0][1]=='O'&&gameBoard[2][1]=='O'){return true;}
+            if(gameBoard[1][0]=='O'&&gameBoard[1][2]=='O'){return true;}
+        }
+        if(gameBoard[0][2]=='O') {
+            if(gameBoard[1][2]=='O'&&gameBoard[2][2]=='O'){return true;}
+        }
+    return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if(this.isInFavorOfO()) {
+            return false;
+        }
+        if(this.isInFavorOfX()) {
+            return false;
+        }
+
+        return true;
     }
 
     public String getWinner() {
+        if(this.isInFavorOfO()) {
+            return "O";
+        }
+        if(this.isInFavorOfX()) {
+            return "X";
+        }
+        if(this.isTie()){return "";}
         return null;
     }
 
